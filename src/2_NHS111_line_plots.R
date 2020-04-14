d5169500cc2c6be5990781f1021888719e0a35da
Emma Vestesson <emma.vestesson@gmail.com>
2020-04-14 11:47:50 +0200

    Try using ggrepel to label data

--- src/2_NHS111_line_plots.R
+++ src/2_NHS111_line_plots.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(THFstyle)
+library(ggrepel)
 
 NHS111 <- readRDS(here::here('data', 'NHS111.RDS'))
 
@@ -14,8 +15,8 @@ national <- NHS111 %>%
   mutate(total_nat=sum(total)) %>% 
   group_by(ageband, date) %>% 
   mutate(total_ageband=sum(total)) %>% 
-  group_by(gender, date) %>% 
-  mutate(total_gender=sum(total)) %>% 
+  group_by(sex, date) %>% 
+  mutate(total_sex=sum(total)) %>% 
   ungroup()
 
 ggplot(national, aes(x=date, y=total_nat)) + 
@@ -33,27 +34,35 @@ national_19_69<- national %>%
     filter(date==max(date), ageband=='19-69 years') %>% 
     pull(total_ageband) %>% 
     unique()
-  
-  ggplot(national) + 
+  national_label <- national %>% 
+    group_by(ageband) %>% 
+    mutate(label=ifelse(date==max(date), ageband, ''))
+  national_label %>% 
+    distinct(date, .keep_all = TRUE) %>% 
+  ggplot(.) + 
     geom_line(aes(x=date, y=total_ageband, colour=ageband, group=ageband)) + 
     geom_point(aes(x=date, y=total_ageband, colour=ageband), size=2, fill='white') +
-    geom_point(aes(x=date, y=total_ageband), color='white', size=1) + theme_THF() + scale_x_date(date_breaks = "1 day") + 
-    scale_y_continuous(labels = scales::comma, limits = c(0, NA)) + scale_colour_THF() + ylab('') + xlab('') +
+    geom_point(aes(x=date, y=total_ageband), color='white', size=1) + 
+    geom_text_repel(aes(x=date, y=total_ageband, label=label, colour=ageband)) + theme_THF() + 
+    scale_x_date(date_breaks = "1 day") + 
+    scale_y_continuous(labels = scales::comma, limits = c(0, NA)) + 
+    scale_colour_THF() + ylab('') + xlab('') +
     theme(plot.title = element_text(hjust = -0.5)) + 
   labs(caption='Source: NHS Digital', title = 'Completed online assessments in NHS 111 Online')
 ggsave(filename=here::here('output', 'NHS111online_ageband.png') ) 
   
   
-# By gender
+# By sex
 label_date=max(national$date)-1
 ggplot(national) + 
-    geom_line(aes(x=date, y=total_gender, colour=gender, group=gender)) + 
-    geom_point(aes(x=date, y=total_gender, colour=gender), size=2, fill='white') +
-    geom_point(aes(x=date, y=total_gender), color='white', size=1) + theme_THF() + scale_x_date(date_breaks = "1 day") + 
+    geom_line(aes(x=date, y=total_sex, colour=sex, group=sex)) + 
+    geom_point(aes(x=date, y=total_sex, colour=sex), size=2, fill='white') +
+    geom_point(aes(x=date, y=total_sex), color='white', size=1) + theme_THF() + 
+    scale_x_date(date_breaks = "2 day") + 
     scale_y_continuous(limits = c(0, NA), labels = scales::comma) + scale_colour_THF() + ylab('') + xlab('') +
     theme(plot.title = element_text(hjust = -0.5), legend.position = 'None') +
     annotate(geom='text', x=label_date, y=10000, label='Male', colour=THF_50pct_light_blue) +
     annotate(geom='text', x=label_date, y=44000, label='Female', colour=THF_red) +
     labs(caption='Source: NHS Digital', title='Completed online assessments in NHS 111 Online')
-ggsave(filename=here::here('output', 'NHS111online_gender.png') ) 
+ggsave(filename=here::here('output', 'NHS111online_sex.png') ) 
   
