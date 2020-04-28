07870d0426b201a1749d84f3c37abda5f92cb1d1
Emma Vestesson <emma.vestesson@gmail.com>
2020-04-28 10:16:29 +0200

    Update downloads file so links are scraped using rvest

--- R/0_download_data.R
+++ R/0_download_data.R
@@ -1,18 +1,25 @@
 library(curl)
-
+library(rvest)  
+library(polite)
+# Grab links from website
+link <- 'https://digital.nhs.uk/data-and-information/publications/statistical/mi-potential-covid-19-symptoms-reported-through-nhs-pathways-and-111-online/latest'
+bow(link)
+page <- read_html(link)
+links <- page %>% html_nodes(css=".block-link") %>% html_attr('href')
+titles <- page %>% html_nodes(css=".block-link") %>% html_attr('title')
+named_links <- purrr::set_names(links, titles)
 # NHS111 online
 
-link_111 <- 'https://files.digital.nhs.uk/9A/7B6495/111%20Online%20Covid-19%20data_2020-04-21.csv'
-
+link_111 <- named_links['111 Online Potential COIVD-19 Open Data']
 
 # scheduler won't run unless path is hard coded (drive mapping problem?)
-destfile_111 <- "M:/COVID-19/COVID19_NHS111/data/original data/NHS111_online.csv"
+destfile_111 <- here::here('data', 'original data', "NHS111_online.csv")
 curl_download(link_111, destfile = destfile_111)
 
 # Pathways
 
-link_pathways <- 'https://files.digital.nhs.uk/82/AE4DE7/NHS%20Pathways%20Covid-19%20data%202020-04-21.csv'
+link_pathways <- named_links['NHS Pathways Potential COVID-19 Open Data']
 
 # scheduler won't run unless path is hard coded (drive mapping problem?)
-destfile_pathways <- "M:/COVID-19/COVID19_NHS111/data/original data/pathways.csv"
+destfile_pathways <- here::here('data', 'original data',"pathways.csv")
 curl_download(link_pathways, destfile = destfile_pathways)
